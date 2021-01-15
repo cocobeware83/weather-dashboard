03155e0c8fd6f728516ec782985d487bbbe3a55b
cory neel <corycneel@gmail.com>
2021-01-15 13:00:45 -0600

    added framework JS to pull weather info for searched city via API key, created VAR to change celcius to farenheit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,37 @@
 //API key for later use =  d6563c1f7289474849eef3ceaf635e1d
+
+$(document).ready(function(){
+
+    var cities = [];
+
+    $("#displayCity").hide();
+    $("#fiveDay").hide();
+
+    //api call for currently searched city
+    function displayCityForecast(city){
+        var apiKey = "d6563c1f7289474849eef3ceaf635e1d";
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            var weatherIcon = response.weather[0].icon;
+            var date = $("<h2>").text(moment().format('l'));
+            var icon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"); 
+            var tempFar = (response.main.temp - 273.15) * 1.80 + 32;
+                
+            $("#displayCityName").text(response.name);
+            $("#displayCityName").append(date);
+            $("#displayCityName").append(icon);
+            $("#displayCityTemp").text(tempFar.toFixed(2) + " \u00B0F");
+            $("#displayCityHumidity").text(response.main.humidity + "%");
+            $("#displayCityWind").text(response.wind.speed + "MPH");
+
+            var lat = response.coord.lat
+            var lon = response.coord.lon
+            queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon; 
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function(response){}
